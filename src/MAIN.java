43a7f6ff3769f32ef595e70018ae0db2c1079ebc
Austin Joy <austinjoy2005@gmail.com>
2026-07-17 17:26:21 +0530

    5 Solved Problems

---
+++ src/MAIN.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+
+
+public class MAIN {
+    public static void main(String[] args) {
+        // int a = 10;
+        // System.out.println(a++);
+        // System.out.println(a);
+        // System.out.println(++a);
+
+        // for(int a = 0; a < 5; a++){
+        //     System.out.println("Hello World");
+        // }
+
+        // int a = 0;
+        // while(a < 5){
+        //     System.out.println("Hello World");
+        //     a++;
+
+        // int a = 10;
+        // do{
+        //     System.out.println(a);
+        //     a++;
+        // }while(a < 5);
+
+        boolean a = true;
+
+        if(a==false){
+            System.out.println("Take an umbrella");
+        }else{
+            System.out.println("No need to take an umbrella");
+        }
+    }
+}
+
---
+++ src/prime.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class prime{
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of test cases: ");
+        int n = sc.nextInt();
+        System.out.print("Enter the numbers to check: ");
+
+        for(int i = 0; i < n; i++) {
+            int t = sc.nextInt();
+            int count = 0;
+
+            for(int div = 1; div <= t; div++)  {
+                if(t%div == 0) {
+                    count++;
+                }
+            }
+            if(count == 2) {
+                System.out.println("Prime");
+            }
+            else {
+                System.out.println("Not Prime");
+            }
+        }
+    }
+    
+
+
+}
