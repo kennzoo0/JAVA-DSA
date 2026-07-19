127190d98121c1781452e4e4f7e6c7898d3b4584
Austin Joy <austinjoy2005@gmail.com>
2026-07-19 23:47:45 +0530

    Array Basic

--- src/Array/Basic.java
+++ src/Array/Basic.java
@@ -1,5 +1,29 @@
 package Array;
+import java.util.*;
 
 public class Basic {
+
+    public static void swap(int[] arr, int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter 5 numbers: ");
+        int[] arr = new int[5];
+
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        swap(arr, 0, 4);
+
+        for(int i = 0; i < arr.length; i++){
+            System.out.println(arr[i]);
+        }
+    }
     
 }
